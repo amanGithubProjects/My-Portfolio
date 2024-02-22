671f421214a48a6d350fb449345ed71f1f9b473c
amanGithubProjects <80571575+amanGithubProjects@users.noreply.github.com>
2024-02-22 16:42:41 +0530

    my portfolio

---
+++ javascript.js
@@ -0,0 +1,6 @@
+var map = L.map('map').setView([22.805618, 86.203110], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
